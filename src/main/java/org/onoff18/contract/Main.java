004034f071ea3661dc588560bc971fbc6f82171e
onoff18 <maxvnik@gmail.com>
2026-07-23 20:37:46 +0300

    создана архитектура пакетов проекта_2
       - Добавлены пакеты: model, repository, service, generator, ui
       - Подготовлена структура для разделения ответственности
    
    Signed-off-by: onoff18 <maxvnik@gmail.com>

--- src/main/java/org/onoff18/contract/Main.java
+++ src/main/java/org/onoff18/contract/Main.java
@@ -1,7 +1,7 @@
 package org.onoff18.contract;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+    static void main(String[] args) {
+        System.out.print("Hello and welcome!");
         }
     }
