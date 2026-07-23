4d429d1d5c866ce49cb5ca18940640488e186a58
onoff18 <maxvnik@gmail.com>
2026-07-23 20:11:35 +0300

    chore: удалил папку .idea из отслеживания Git
       - Добавил .idea/ в .gitignore
       - Остановил отслеживание служебных файлов IDEA
    
    Signed-off-by: onoff18 <maxvnik@gmail.com>

--- src/main/java/org/onoff18/contract/Main.java
+++ src/main/java/org/onoff18/contract/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.onoff18.contract;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
