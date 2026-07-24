b60c17dbfcf8127ad499fdebc51c4a8eafd0a822
onoff18 <maxvnik@gmail.com>
2026-07-24 10:00:04 +0300

    refactor: внедрено профессиональное логирование SLF4J
       - Добавлены зависимости slf4j-api и slf4j-simple
       - Заменен System.out.println на log.info
       - Заменен e.printStackTrace на log.error
       - Использована аннотация Lombok @Slf4j
    
    Signed-off-by: onoff18 <maxvnik@gmail.com>

---
+++ src/main/java/org/onoff18/contract/App.java
@@ -0,0 +1,51 @@
+package org.onoff18.contract;
+
+import lombok.extern.slf4j.Slf4j;
+import org.onoff18.contract.model.Personal;
+import org.onoff18.contract.repository.PersonalRepository;
+
+import java.util.List;
+
+@Slf4j // Магия Lombok: автоматически создает переменную log
+public class App {
+    public static void main(String[] args) {
+        try {
+            // 1. Создаем репозиторий
+            PersonalRepository repository = new PersonalRepository();
+
+            // 2. Создаем тестового самозанятого
+            Personal personal = Personal.builder()
+                    .name("Иванов Иван Иванович")
+                    .passport("1234 567890 10101")
+                    .address("г. Москва, ул. Пушкина, д. 10")
+                    .inn("123456789012")
+                    .npdRegistrationCity("г. Москва")
+                    .bankAccount("40817810099910004312")
+                    .bankName("ПАО Сбербанк")
+                    .correspondentAccount("30101810400000000225")
+                    .bankIdentifierCode("044525225")
+                    .phone("+79001234567")
+                    .build();
+
+            // 3. Сохраняем в JSON
+            log.info("--- Начало сохранения данных ---");
+            repository.save(List.of(personal));
+
+            // 4. Загружаем из JSON
+            log.info("--- Начало загрузки данных ---");
+            List<Personal> loadedPersonals = repository.load();
+
+            // 5. Выводим результат
+            for (Personal p : loadedPersonals) {
+                log.info("Загружен пользователь: {}", p.getName());
+                log.info("ИНН: {}, Банк: {}", p.getInn(), p.getBankName());
+            }
+
+            log.info("--- Операция успешно завершена ---");
+
+        } catch (Exception e) {
+            // Профессиональная обработка ошибки
+            log.error("Произошла критическая ошибка при работе с репозиторием", e);
+        }
+    }
+}
